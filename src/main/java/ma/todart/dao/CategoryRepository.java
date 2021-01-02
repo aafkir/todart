e170ee10308f627eb9b6fc84d15ea2556eba8b4a
Ahmed Afkir <a.afkkir@gmail.com>
2021-01-02 23:39:20 +0100

    second commit

---
+++ src/main/java/ma/todart/dao/CategoryRepository.java
@@ -0,0 +1,10 @@
+package ma.todart.dao;
+
+import ma.todart.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/ma/todart/dao/ProductRepository.java
@@ -0,0 +1,20 @@
+package ma.todart.dao;
+
+import ma.todart.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @RestResource(path="/selectedProducts")
+    public List<Product> findBySelectedIsTrue();
+
+    @RestResource(path="/productsByKeyword")
+    public List<Product> findByNameContains(@Param("mc") String mc);
+
+}
